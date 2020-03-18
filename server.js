7fc1679efd35585ca42fccc7a915acbe9df13a8c
Anubhaw Choudhary <46886172+anubhawchoudhary@users.noreply.github.com>
2020-03-18 15:51:34 +0530

    Update server.js
    added typing status

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-// var http = require('http').Server(app);
-// var io=require('socket.io')(http);
 
 var mongoose = require('mongoose');
 app.use(express.static(__dirname));
@@ -29,13 +27,16 @@ var server = app.listen(3000,() =>{
 });
 var io=socket(server);
 
-io.on('connection',()=>{
-    console.log('a user is connected');
+io.on('connection',(socket)=>{
+    console.log('new person joined chatroom');
+
+
+    socket.on('typing',(name)=>{
+        io.emit('typing', name);
+    })
 })
 
-// io.on('typing',(name)=>{
-//     socket.broadcast.emit('typing', name);
-// })
+
 
 
 app.get('/messages',(req,res)=>{
